1857dba36098fab1f865e5b6020b573d42bd2b34
Srinitha Mulagundla <srinithareddy564@gmail.com>
2026-06-28 00:34:25 +0530

    Completed day 2 : all Typescript excerices done

---
+++ Ayurvena-Bootcamp/day2-typescript/hour15-16-ayurvena-types/types/responses.ts
@@ -0,0 +1,27 @@
+export interface ApiResponse<T> {
+    success : boolean;
+    data : T;
+    message : string;
+    statusCode : number;
+}
+export interface ApiError {
+    success : false;
+    error : string;
+    message : string;
+    statusCode : number;
+}
+export interface PaginatedResponse<T> {
+    success : boolean;
+    count : number;
+    data : T[];
+}
+export interface Searchresponse<T> {
+    success : boolean;
+    keyword : string;
+    results : T[];
+}
+export interface DashboardResponse<T> {
+    success : boolean;
+    data : T;
+    generateAt : Date;
+}
